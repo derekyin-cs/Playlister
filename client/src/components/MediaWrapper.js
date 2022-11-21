ad71b787543457b317ec78f06bfb5689e5df0f6d
Derek <derek.yin@stonybrook.edu>
2022-11-21 17:23:37 -0500

    Player, Comment Buttons

--- client/src/components/MediaWrapper.js
+++ client/src/components/MediaWrapper.js
@@ -1,5 +1,29 @@
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
 export default function MediaWrapper() {
+
+
+    // conditionally render player or comments based on which tab is pressed
+    // state variable for which tab is pressed
+
+    // box for comment section
+    // box for player
+    let comments = "";
+
+    if (true) {
+        comments = (
+            <Box sx={{ bgcolor: 'background.paper', p: 2, flexGrow: 1 }}>
+                <Typography variant="h5" component="div" gutterBottom>
+                    Comments
+                </Typography>
+                <Typography variant="body1">
+                    Comments go here
+                </Typography>
+            </Box>
+        )
+    } 
+
 
     return (
         // two side by side buttons, one named player and one named comments
@@ -7,23 +31,14 @@ export default function MediaWrapper() {
 
         // player button
         <div id="media-wrapper">
-            <div id="media-wrapper-player">
-                <div id="media-wrapper-player-heading">
-                    <Typography variant="h2">Player</Typography>
-                </div>
-                <div id="media-wrapper-player-body">
-                    placeholder
-                </div>
-            </div>
-                <div id="media-wrapper-comments-heading">
-                    <Typography variant="h2">Comments</Typography>
-                </div>
 
-            <div id="media-wrapper-comments">
-                <div id="media-wrapper-comments-body">
-                    placeholder
-                </div>
+            <div id = "media-wrapper-tabs">
+                <Button variant="contained" color="primary">Player</Button>
+                <Button variant="contained" color="primary">Comments</Button>
             </div>
+
+            {comments}
+            
 
         </div>
 
